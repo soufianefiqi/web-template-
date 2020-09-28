f8e57897a9977ad9733455b4e4b4ab6cc26d8e8c
soufianefiqi <72028348+soufianefiqi@users.noreply.github.com>
2020-09-28 21:46:49 +0700

    Create plt.php

---
+++ plt.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Website</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+     
+    <div class="badan-utama">
+        
+        <header>
+            <h2>MY WEBSITE</h2>
+        </header>
+ 
+        <nav class="navigasi">
+            <ul>
+                <li><a class="active" href="home.html">Home</a></li>
+		        <li><a class="active" href="#">About Me</a></li>
+				<li><a class="active" href='#'>WFH </a></li>
+				<li><a class="active" href="#">Facilities</a></li>
+           </ul>
+        </nav>
+ 
+        <div class="banner">
+            <img src="image/profil-back.jpg">
+        </div>
+		<div class = "profil">
+		    <img src="image/profil.jpg">
+        </div>
+		<marquee width="800" height="20">-----------------------------Welcome------------------------------</marquee><br/>
+		<div class="gif">
+		<img src ="image/tenor.gif" width="200" height="200"></div>
+        <div class="menu-kiri">
+            <div class="kotak">
+                <h3>Profil</h3>
+				<img src="image/hak.jpeg" width="30px" height="30px" align="left">
+				<div class="text">
+                <h4>
+                    Soufiane Fiqi Vindia </br>
+					11180910000055</br>
+					<br></br>
+					Mahasiswi UIN Jakarta</br>
+					Prodi Teknik Informatika
+					<br></br>
+					No telp +6288123456</br>
+					<a href="mailto:soufiane.fiqi18@mhs.uinjkt.ac.id" title="kirim email ke admin">Hubungi Saya</a>
+                </h4>
+				</div>
+				
+ 
+                
+            </div>
+        </div>
+ 
+        <div class="menu-tengah">
+            <div class="kotak">
+                <h3>Pusat Laboratorium UIN Jakarta</h3>
+                <center><img src="image/plt.jpg" alt="perpus" width="300" height="300" ></center>
+                <p>Jika ada Fakultas Sains dan Teknologi, maka tidak akan lengkap tanpa adanya laboratorium. Laboratorium di UIN Jakarta ini 
+                    memiliki 6 lantai, dimana dari lantai 5-6 khusus jurusan Teknik Informatika, Sistem Informasi. dan matematika saja karena hampir di setiap ruangannya 
+                    menyediakan komputer untuk masing-masing bangku. Di lantai 5-6 terdapat ruangan Lab.Aplikasi, Lab.Jaringan, Lab.Matematika, dan masih banyak lagi. 
+                    </p>
+                
+                </p>
+              
+                <a class="tombol tombol-lengkap" href="#" onclick="alert('Eitss, Tidak Ada......')">Selengkapnya</a>
+            </div>
+        </div>
+
+        <div class="calendar">
+            <div class="month-indicator">
+              <time datetime="2019-02"> September 2020 </time>
+            </div>
+            <div class="day-of-week">
+              <div>Su</div>
+              <div>Mo</div>
+              <div>Tu</div>
+              <div>We</div>
+              <div>Th</div>
+              <div>Fr</div>
+              <div>Sa</div>
+            </div>
+            <div class="date-grid">
+              <button>
+                <time datetime="2019-02-01">1</time>
+              </button>
+              <button>
+                <time datetime="2019-02-02">2</time>
+              </button>
+              <button>
+                <time datetime="2019-02-03">3</time>
+              </button>
+              <button>
+                <time datetime="2019-02-04">4</time>
+              </button>
+              <button>
+                <time datetime="2019-02-05">5</time>
+              </button>
+              <button>
+                <time datetime="2019-02-06">6</time>
+              </button>
+              <button>
+                <time datetime="2019-02-07">7</time>
+              </button>
+              <button>
+                <time datetime="2019-02-08">8</time>
+              </button>
+              <button>
+                <time datetime="2019-02-09">9</time>
+              </button>
+              <button>
+                <time datetime="2019-02-10">10</time>
+              </button>
+              <button>
+                <time datetime="2019-02-11">11</time>
+              </button>
+              <button>
+                <time datetime="2019-02-12">12</time>
+              </button>
+              <button>
+                <time datetime="2019-02-13">13</time>
+              </button>
+              <button>
+                <time datetime="2019-02-14">14</time>
+              </button>
+              <button>
+                <time datetime="2019-02-15">15</time>
+              </button>
+              <button>
+                <time datetime="2019-02-16">16</time>
+              </button>
+              <button>
+                <time datetime="2019-02-17">17</time>
+              </button>
+              <button>
+                <time datetime="2019-02-18">18</time>
+              </button>
+              <button>
+                <time datetime="2019-02-19">19</time>
+              </button>
+              <button>
+                <time datetime="2019-02-20">20</time>
+              </button>
+              <button>
+                <time datetime="2019-02-21">21</time>
+              </button>
+              <button>
+                <time datetime="2019-02-22">22</time>
+              </button>
+              <button>
+                <time datetime="2019-02-23">23</time>
+              </button>
+              <button>
+                <time datetime="2019-02-24">24</time>
+              </button>
+              <button>
+                <time datetime="2019-02-25">25</time>
+              </button>
+              <button>
+                <time datetime="2019-02-26">26</time>
+              </button>
+              <button>
+                <time datetime="2019-02-27">27</time>
+              </button>
+              <button>
+                <time datetime="2019-02-28">28</time>
+              </button>
+              <button>
+                <time datetime="2019-02-28">29</time>
+              </button>
+              <button>
+                <time datetime="2019-02-28">30</time>
+              </button>
+            </div>
+          </div>
+ 
+ 		
+ 
+         
+            <div class="kotak2">
+                <h3>Berita Terbaru</h3>
+ 
+                <nav class="menu-artikel">
+                    <ul>
+                        <li><a href="https://www.liputan6.com/tag/wfh">Suasana WFH Terkini</a></li>
+                        <li><a href="https://www.traveloka.com/en-id/explore/tips/5-kegiatan-yang-dapat-kamu-lakukan-saat-wfh-dan-social-distancing/27185">Kegiatan Selama WFH</a></li>
+                        <li><a href="https://www.alodokter.com/ketahui-cara-untuk-mencegah-penularan-virus-corona">Cara Mencegah Virus Corona</a></li>
+                        <li><a href="https://www.bluestacks.com/apps/action/among-us-on-pc.html">Main Among US</a></li>
+                        <li><a href="https://www.tribunnews.com/techno/2020/09/26/cara-bermain-game-among-us-di-hp-android-dan-ios-simak-aturannya-berikut">Cara Main Among Us</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="kotak-kanan">
+                <div class="kotak3">
+                    <h3>Fasilitas UIN Jakarta</h3>
+                    <a href="fst.html" title="klik fst" target="_self">
+                        <img src="image/fst.jpeg" alt="FST" width="100px" height="100px"  border="2px solid  black" align="left">
+                        <p>Fakultas Sains Dan Teknologi </p></a></br></br></br></br>
+                        <a href="plt.html" title="klik plt" target="_self">
+                            <img src="image/plt.jpg" alt="FST" width="100px" height="100px"  border="2px solid  black" align="left">
+                            <p>Pusat Laboratorium Teknologi</p></a></br></br></br></br>
+                            <a href="perpus.html" title="klik perpus" target="_self">
+                                <img src="image/perpus.jpg" alt="FST" width="100px" height="100px"  border="2px solid  black" align="left">
+                                <p>Perpustakaan UIN Jakarta</p></a>
+        </div>
+            </div>
+  
+ 
+        <footer>
+            @copyright 2020 || by Soufiane
+        </footer>
+   </div>
+    </div>
+ 
+</body>
+</html>
