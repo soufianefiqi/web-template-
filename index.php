ab6eacefcfbed677c5150290a579326faca65462
soufianefiqi <72028348+soufianefiqi@users.noreply.github.com>
2020-09-28 21:45:18 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Website</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+     
+    <div class="badan-utama">
+        
+        <header>
+            <h2>MY WEBSITE</h2>
+        </header>
+ 
+        <nav class="navigasi">
+            <ul>
+                <li><a class="active" href="#home">Home</a></li>
+		        <li><a class="active" href="#introduce">About Me</a></li>
+				<li><a class="active" href="#WFH">WFH </a></li>
+				<li><a class="active" href="#uin">Facilities</a></li>
+           </ul>
+        </nav>
+ 
+        <div class="banner">
+            <img src="image/profil-back.jpg">
+        </div>
+		<div class = "profil">
+		    <img src="image/profil.jpg">
+        </div>
+		<marquee width="800" height="20">-----------------------------Welcome------------------------------</marquee><br/>
+		<div class="gif">
+		<img src ="image/tenor.gif" width="200" height="200"></div>
+        <div class="menu-kiri">
+            <div class="kotak">
+                <h3>Profil</h3>
+				<img src="image/hak.jpeg" width="30px" height="30px" align="left">
+				<div class="text">
+                <h4>
+                    Soufiane Fiqi Vindia </br>
+					11180910000055</br>
+					<br></br>
+					Mahasiswi UIN Jakarta</br>
+					Prodi Teknik Informatika
+					<br></br>
+					No telp +6288123456</br>
+					<a href="mailto:soufiane.fiqi18@mhs.uinjkt.ac.id" title="kirim email ke admin">Hubungi Saya</a>
+                </h4>
+				</div>
+				
+ 
+                
+            </div>
+        </div>
+ 
+        <div class="menu-tengah">
+            <div class="kotak">
+                <h3 id="introduce">Introduce</h3>
+                
+                <p>
+                 Website ini dibuat untuk memenuhi tugas mata kuliah <i>Cloud Computing</i>. Saya mahasiswi SMT 5 
+				 jurusan teknik informatika di UIN Syarif Hidayatullah Jakarta. Terkait mengenai program yang lumayan sudah lama 
+				 diterapkan, yaitu kuliah online atau <i>daring</i> dikarenakan pandemic virus corona sejak bulan Maret tahun 2020
+				 segala aktivitas yang biasa dilakukan di luar menjadi di dalam rumah. Sudah 7 bulan sejak diterapkannya <i>daring</i>
+				 dan tentu saja selain kuliah <i>daring</i> ada beberapa hal yang saya lakukan di rumah. Selain <i>rebahan</i> terutama
+				 menonton film di laptop kesayangan, bermain game online, dll. </br>
+				 </br>----end----
+                </p>
+              
+                <a class="tombol tombol-lengkap" href="#" onclick="alert('Eitss, Tidak Ada......')">Selengkapnya</a>
+            </div>
+        </div>
+
+        <div class="calendar">
+            <div class="month-indicator">
+              <time datetime="2019-02"> September 2020 </time>
+            </div>
+            <div class="day-of-week">
+              <div>Su</div>
+              <div>Mo</div>
+              <div>Tu</div>
+              <div>We</div>
+              <div>Th</div>
+              <div>Fr</div>
+              <div>Sa</div>
+            </div>
+            <div class="date-grid">
+              <button>
+                <time datetime="2019-02-01">1</time>
+              </button>
+              <button>
+                <time datetime="2019-02-02">2</time>
+              </button>
+              <button>
+                <time datetime="2019-02-03">3</time>
+              </button>
+              <button>
+                <time datetime="2019-02-04">4</time>
+              </button>
+              <button>
+                <time datetime="2019-02-05">5</time>
+              </button>
+              <button>
+                <time datetime="2019-02-06">6</time>
+              </button>
+              <button>
+                <time datetime="2019-02-07">7</time>
+              </button>
+              <button>
+                <time datetime="2019-02-08">8</time>
+              </button>
+              <button>
+                <time datetime="2019-02-09">9</time>
+              </button>
+              <button>
+                <time datetime="2019-02-10">10</time>
+              </button>
+              <button>
+                <time datetime="2019-02-11">11</time>
+              </button>
+              <button>
+                <time datetime="2019-02-12">12</time>
+              </button>
+              <button>
+                <time datetime="2019-02-13">13</time>
+              </button>
+              <button>
+                <time datetime="2019-02-14">14</time>
+              </button>
+              <button>
+                <time datetime="2019-02-15">15</time>
+              </button>
+              <button>
+                <time datetime="2019-02-16">16</time>
+              </button>
+              <button>
+                <time datetime="2019-02-17">17</time>
+              </button>
+              <button>
+                <time datetime="2019-02-18">18</time>
+              </button>
+              <button>
+                <time datetime="2019-02-19">19</time>
+              </button>
+              <button>
+                <time datetime="2019-02-20">20</time>
+              </button>
+              <button>
+                <time datetime="2019-02-21">21</time>
+              </button>
+              <button>
+                <time datetime="2019-02-22">22</time>
+              </button>
+              <button>
+                <time datetime="2019-02-23">23</time>
+              </button>
+              <button>
+                <time datetime="2019-02-24">24</time>
+              </button>
+              <button>
+                <time datetime="2019-02-25">25</time>
+              </button>
+              <button>
+                <time datetime="2019-02-26">26</time>
+              </button>
+              <button>
+                <time datetime="2019-02-27">27</time>
+              </button>
+              <button>
+                <time datetime="2019-02-28">28</time>
+              </button>
+              <button>
+                <time datetime="2019-02-28">29</time>
+              </button>
+              <button>
+                <time datetime="2019-02-28">30</time>
+              </button>
+            </div>
+          </div>
+ 
+ 		<div class="menu-wfh">
+		 <div class="kotak">
+			<h3 id="WFH">Kegiatan Selama WFH</h3>
+            <p>WFH adalah singkatan dari <i>work from home</i> yang berarti bekerja dari rumah. 
+                Secara umum biasanya <i>work from home</i> diartikan dengan cara kerja karyawan yang berada di luar kantor.
+                Entah dari rumah, dari cafe atau restoran sesuai dengan keinginan karyawan. 
+                Sistem kerja wfh memang memiliki fleksibilitas yang tinggi. 
+                Hal ini guna mendukung keseimbangan karyawan antara pekerjaan dan kehidupan.  </p>
+            <p>Istilah WFH baru <i>trending</i> sejak pandemic virus corona menyerah hampir seluruh negara di dunia. 
+                Virus corona mengharukan kita untuk selalu beraktifitas di dalam kediaman masing-masing, guna mentolerir penularan. 
+                dikarenakan belum adanya vaksin yang tepat untuk membasmi virus tersebut, maka hampir semua pemerintahan di setiap negara  
+                menegaskan untuk melakukan WFH ini. </p>
+            <p>untuk mengenang suasana WFH ini, banyak hal yang dilakukan orang-orang. Termasuk saya, kesempatan untuk melatih <i>skill-skill</i>
+                bisa didapatkan, walaupun dalam keadaan terperangkap di rumah. Seperti bermain gitar, beberapa telah saya lewati untuk belajar cara 
+                memainkan gitar. Belajar dari paling dasar, yaitu kunci dasar gitar dan dilanjut memainkan beberapa lagu. Saya pun berniat melakukan 
+                <i>cover-cover</i>lagu, bersama dengan teman saya sebagai vokalisnya. 
+                berikut adalah video cover : </br>
+            <i>enjoy the music!!!</i></p>
+            <video width="500" height="auto" controls>
+                <source src="video/video cover.mp4" type="video/mp4"/>
+                Browser Anda Tidak MenSupport HTML 5 Video</video>
+                <p>Ini hanya untuk referensi, bayangkan saja jika saya dan teman saya yang melakukan cover akustiknya :))</p> 
+                <p>Selain belajar bermain gitar, kegiatan WFH ini bisa melakukan olahraga juga <i>lho</i>. Gimana caranya?
+                    dampak pandemic corona ini ternyata tidak hanya negatif saja, bahkan bisa berdampak positif. salah satunya dapat 
+                    meningkatkan kekreativitas para individu-individu di dunia. Seperti olahraga secara online dengan stiker <i>whatsapp</i>.
+                </br>seperti pada contoh video di bawah ini : </p>
+                <video width="500" height="auto" controls>
+                <source src="video/senam skj.mp4" type="video/mp4"/>
+                Browser Anda Tidak MenSupport HTML 5 Video</video>
+                <p>Dan masih banyak lagi yang dapat dilakukan selama WFH</p>
+         </div>
+		 </div>
+            <div class="kotak2">
+                <h3>Berita Terbaru</h3>
+ 
+                <nav class="menu-artikel">
+                    <ul>
+                        <li><a href="https://www.liputan6.com/tag/wfh">Suasana WFH Terkini</a></li>
+                        <li><a href="https://www.traveloka.com/en-id/explore/tips/5-kegiatan-yang-dapat-kamu-lakukan-saat-wfh-dan-social-distancing/27185">Kegiatan Selama WFH</a></li>
+                        <li><a href="https://www.alodokter.com/ketahui-cara-untuk-mencegah-penularan-virus-corona">Cara Mencegah Virus Corona</a></li>
+                        <li><a href="https://www.bluestacks.com/apps/action/among-us-on-pc.html">Main Among US</a></li>
+                        <li><a href="https://www.tribunnews.com/techno/2020/09/26/cara-bermain-game-among-us-di-hp-android-dan-ios-simak-aturannya-berikut">Cara Main Among Us</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="kotak-kanan">
+                <div class="kotak3">
+                    <h3 id="uin">Fasilitas UIN Jakarta</h3>
+                    <a href="fst.html" title="klik fst" target="_self">
+                        <img src="image/fst.jpeg" alt="FST" width="100px" height="100px"  border="2px solid  black" align="left">
+                        <p>Fakultas Sains Dan Teknologi </p></a></br></br></br></br>
+                        <a href="plt.html" title="klik plt" target="_self">
+                            <img src="image/plt.jpg" alt="FST" width="100px" height="100px"  border="2px solid  black" align="left">
+                            <p>Pusat Laboratorium Teknologi</p></a></br></br></br></br>
+                            <a href="perpus.html" title="klik perpus" target="_self">
+                                <img src="image/perpus.jpg" alt="FST" width="100px" height="100px"  border="2px solid  black" align="left">
+                                <p>Perpustakaan UIN Jakarta</p></a>
+        </div>
+        </div>
+        <footer>
+            @copyright 2020 || by Soufiane
+        </footer>
+ 
+    </div>
+ 
+</body>
+</html>
